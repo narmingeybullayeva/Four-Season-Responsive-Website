f170ea7f78217d7f02ff18e428b0e4eb7cdc7c18
Narmin-G-M <narmin.g.mammadli@gmail.com>
2024-02-17 22:30:38 +0400

    troubleshooting

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const toggle = document.getElementById("icon")
 const list = document.getElementById("nav__list")
+const nav = document.getElementById("nav")
 
 toggle.addEventListener("click", () => {
   list.classList.toggle("list__show") // Use toggle instead of add
   icon.style.opacity = list.classList.contains("list__show") ? 1 : ""
+  nav.style.backgroundColor = list.classList.contains("list__show")
+    ? "#FEE7BE"
+    : ""
 })
